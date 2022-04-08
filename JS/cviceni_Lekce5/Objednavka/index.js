34c1e5e3debbfe5acf8fb01dbae3127aa5684711
BukovskaPavla <bukovskapavla@gmail.com>
2022-04-08 18:01:51 +0200

    zip

--- JS/cviceni_Lekce5/Objednavka/index.js
+++
@@ -1,7 +0,0 @@
-const formular = document.querySelector("#registration")
-
-
-formular.addEventListener("submit", (udalost) => {
-    udalost.preventDefault() //at te neneapadne stranku znova nacist
-    formular.textContent = `Objednávka odeslána ke zpracování.`
-})
--- JS/cviceni_Lekce5/formular/index.js
+++
@@ -1,7 +0,0 @@
-const prvniFormular = document.querySelector("#prvniFormular")
-const firstName = document.querySelector("#firstName")
-
-prvniFormular.addEventListener("submit", (udalost) => {
-    udalost.preventDefault() //at te neneapadne stranku znova nacist
-    alert(firstName.value)
-})
